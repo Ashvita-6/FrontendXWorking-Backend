8cc5959e286fa94e89075b5d4dbef7c6ad510d0e
Ashvita-6 <r.ashvita.mec22@itbhu.ac.in>
2025-10-22 12:08:45 +0530

    prod ready

--- api/index.js
+++ api/index.js
@@ -7,7 +7,17 @@ import 'dotenv/config';
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-app.use(cors()); 
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true })); // Good practice to add this too
 
